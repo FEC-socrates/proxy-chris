9aa34105133e2fd2088faa5407876e2e5db6a7ff
ccades <hellocaden@gmail.com>
2019-01-03 20:53:16 -0800

    proxy webpack builds without error and renders all components

--- buySell/webpack.config.js
+++ buySell/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        include: SRC_DIR,
+        exclude: /(node_modules)/,
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-react', '@babel/preset-env'],
